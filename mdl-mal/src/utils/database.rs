5bcf5b672032d60ca672725c6b46fd31db23db4d
nandanjp <nandan.jp17@gmail.com>
2024-02-25 01:00:53 -0700

    Added a database helper file. Need to still add the request and response payloads to each of the models, and then the handler logic can be worked on

---
+++ mdl-mal/src/utils/database.rs
@@ -0,0 +1,7 @@
+use axum::response::IntoResponse;
+use http::StatusCode;
+use mongodb::{Client, Collection};
+
+pub async fn get_collection<T>(client: &Client, collection: &'static str) -> Collection<T> {
+    client.database("mdl_mal").collection::<T>(collection)
+}
