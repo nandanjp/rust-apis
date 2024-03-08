3aa115411fc472f6dc1a923ad343fc2be79555b7
nandanjp <nandan.jp17@gmail.com>
2024-03-08 11:20:39 -0700

    was testing postgres formatter

--- pokemon-team-builder/migrations/20240306051106_first.up.sql
+++ pokemon-team-builder/migrations/20240306051106_first.up.sql
@@ -1,106 +1,196 @@
 -- defining types
-create type Game as enum ('redblue', 'goldsilver', 'rubysapphire', 'diamondpearl', 'blackwhite', 'xy', 'sunmoon', 'swordshield', 'scarletviolet');
-create type Gender as enum ('male', 'female', 'unknown');
-create type Nature as enum ('adamant', 'bashful', 'bold', 'brave', 'calm', 'careful', 'docile', 'gentle', 'hardy', 'hasty', 'impish', 'jolly', 'lax', 'lonely', 'mild', 'modest', 'naive', 'naughty', 'quiet', 'quirky', 'rash', 'relaxed', 'sassy', 'serious', 'timid');
-create type Region as enum ('kanto', 'johto', 'hoenn', 'hisui', 'sinnoh', 'unova', 'kalos', 'alola', 'galar', 'paldea');
-create type Stat as enum ('hp', 'attack', 'defense', 'spattack', 'spdefense', 'speed');
-create type Tier as enum ('ag', 'uber', 'ou', 'uu', 'ru', 'nu', 'pu', 'zu');
-create type Type as enum ('normal', 'fire', 'water', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug', 'ghost', 'steel', 'dragon', 'dark', 'fairy');
+CREATE TYPE Game AS enum (
+    'redblue',
+    'goldsilver',
+    'rubysapphire',
+    'diamondpearl',
+    'blackwhite',
+    'xy',
+    'sunmoon',
+    'swordshield',
+    'scarletviolet'
+);
+
+CREATE TYPE Gender AS enum (
+    'male',
+    'female',
+    'unknown'
+);
+
+CREATE TYPE Nature AS enum (
+    'adamant',
+    'bashful',
+    'bold',
+    'brave',
+    'calm',
+    'careful',
+    'docile',
+    'gentle',
+    'hardy',
+    'hasty',
+    'impish',
+    'jolly',
+    'lax',
+    'lonely',
+    'mild',
+    'modest',
+    'naive',
+    'naughty',
+    'quiet',
+    'quirky',
+    'rash',
+    'relaxed',
+    'sassy',
+    'serious',
+    'timid'
+);
+
+CREATE TYPE Region AS enum (
+    'kanto',
+    'johto',
+    'hoenn',
+    'hisui',
+    'sinnoh',
+    'unova',
+    'kalos',
+    'alola',
+    'galar',
+    'paldea'
+);
+
+CREATE TYPE Stat AS enum (
+    'hp',
+    'attack',
+    'defense',
+    'spattack',
+    'spdefense',
+    'speed'
+);
+
+CREATE TYPE Tier AS enum (
+    'ag',
+    'uber',
+    'ou',
+    'uu',
+    'ru',
+    'nu',
+    'pu',
+    'zu'
+);
+
+CREATE TYPE Type AS enum (
+    'normal',
+    'fire',
+    'water',
+    'grass',
+    'flying',
+    'fighting',
+    'poison',
+    'electric',
+    'ground',
+    'rock',
+    'psychic',
+    'ice',
+    'bug',
+    'ghost',
+    'steel',
+    'dragon',
+    'dark',
+    'fairy'
+);
 
 -- defining tables
-create table if not exists users (
-    id serial primary key,
-    name varchar (255) not null,
-    email varchar (255) not null unique,
-    password varchar (100) not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
+    password varchar(100) NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists ability (
-    id serial primary key,
-    name varchar (100) not null unique,
-    is_main_series boolean default true,
-    generation Game not null,
-    effect_entry text not null,
-    effect_changes text not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp
+CREATE TABLE IF NOT EXISTS ability (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL UNIQUE,
+    is_main_series boolean DEFAULT TRUE,
+    generation Game NOT NULL,
+    effect_entry text NOT NULL,
+    effect_changes text NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists generation (
-    id serial primary key,
-    name varchar(100) not null unique,
-    main_region Region not null,
+CREATE TABLE IF NOT EXISTS generation (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL UNIQUE,
+    main_region Region NOT NULL,
     types Type[],
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists pokedex (
-    id serial primary key,
-    name varchar(100) not null unique,
-    is_main_series boolean default true,
-    description text not null,
-    region Region not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp
+CREATE TABLE IF NOT EXISTS pokedex (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL UNIQUE,
+    is_main_series boolean DEFAULT TRUE,
+    description text NOT NULL,
+    region Region NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists move (
-    id serial primary key,
-    name varchar(100) not null unique,
+CREATE TABLE IF NOT EXISTS pokemon_move (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL UNIQUE,
     accuracy integer,
     effect_change integer,
     pp integer,
-    priority integer not null,
+    priority integer NOT NULL,
     power integer,
-    type Type not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp
+    type Type NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists pokemon (
-    id serial primary key,
-    name varchar(100) not null unique,
+CREATE TABLE IF NOT EXISTS pokemon (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL UNIQUE,
     base_experience integer,
-    height real not null,
-    weight real not null,
-    primary_type Type not null,
+    height real NOT NULL,
+    weight real NOT NULL,
+    primary_type Type NOT NULL,
     secondary_type Type,
-    primary_ability integer not null,
+    primary_ability integer NOT NULL,
     secondary_ability integer,
     hidden_ability integer,
-    is_main_series boolean default true,
-    pokedex integer not null,
-    origin_region Region not null,
-    games Game[] not null,
+    is_main_series boolean DEFAULT TRUE,
+    pokedex integer NOT NULL,
+    origin_region Region NOT NULL,
+    games Game[] NOT NULL,
     form_names varchar(100)[],
-    is_mythical boolean default false,
-    is_legendary boolean default false,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp,
-
-    foreign key (primary_ability) references ability(id),
-    foreign key (secondary_ability) references ability(id),
-    foreign key (hidden_ability) references ability(id),
-    foreign key (pokedex) references pokedex(id)
+    is_mythical boolean DEFAULT FALSE,
+    is_legendary boolean DEFAULT FALSE,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (primary_ability) REFERENCES ability (id),
+    FOREIGN KEY (secondary_ability) REFERENCES ability (id),
+    FOREIGN KEY (hidden_ability) REFERENCES ability (id),
+    FOREIGN KEY (pokedex) REFERENCES pokedex (id)
 );
 
-create table if not exists pokemonstat (
-    id serial primary key,
-    pokemon integer not null,
-    kind Stat not null,
-    value integer not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp,
-
-    foreign key (pokemon) references pokemon(id)
+CREATE TABLE IF NOT EXISTS pokemonstat (
+    id serial PRIMARY KEY,
+    pokemon integer NOT NULL,
+    kind Stat NOT NULL,
+    value integer NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (pokemon) REFERENCES pokemon (id)
 );
 
-create table if not exists pokemonsprites (
-    id serial primary key,
-    pokemon integer not null,
+CREATE TABLE IF NOT EXISTS pokemonsprites (
+    id serial PRIMARY KEY,
+    pokemon integer NOT NULL,
     front_default text,
     front_shiny text,
     front_female text,
@@ -109,43 +199,39 @@ create table if not exists pokemonsprites (
     back_shiny text,
     back_female text,
     back_shiny_female text,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp,
-
-    foreign key (pokemon) references pokemon(id)
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (pokemon) REFERENCES pokemon (id)
 );
 
-create table if not exists team (
-    id serial primary key,
-    name varchar(100) not null,
-    description text not null,
-    user_id integer not null,
-    tier Tier not null,
-    is_favourite boolean default false,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp,
-
-    foreign key (user_id) references users (id)
+CREATE TABLE IF NOT EXISTS team (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    description text NOT NULL,
+    user_id integer NOT NULL,
+    tier Tier NOT NULL,
+    is_favourite boolean DEFAULT FALSE,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-create table if not exists team_pokemon (
-    id serial primary key,
-    team integer not null,
-    pokemon integer not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp,
-
-    foreign key (team) references team(id),
-    foreign key (pokemon) references pokemon(id)
+CREATE TABLE IF NOT EXISTS team_pokemon (
+    id serial PRIMARY KEY,
+    team integer NOT NULL,
+    pokemon integer NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (team) REFERENCES team (id),
+    FOREIGN KEY (pokemon) REFERENCES pokemon (id)
 );
 
-create table if not exists move_pokemon (
-    id serial primary key,
-    move integer not null,
-    pokemon integer not null,
-    created_at timestamptz default current_timestamp,
-    updated_at timestamptz default current_timestamp,
-
-    foreign key (move) references move(id),
-    foreign key (pokemon) references pokemon(id)
+CREATE TABLE IF NOT EXISTS move_pokemon (
+    id serial PRIMARY KEY,
+    move integer NOT NULL,
+    pokemon integer NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (MOVE) REFERENCES MOVE (id),
+    FOREIGN KEY (pokemon) REFERENCES pokemon (id)
 );
