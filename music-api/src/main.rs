38d5ff4252831a3494750c241a7348fbd84105b3
nandanjp <nandan.jp17@gmail.com>
2024-03-08 16:22:25 -0700

    added crates and initialized and .env file

--- music-api/src/main.rs
+++ music-api/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv::ok();
+
+    let database = std::env::var("DATABASE_URL").expect("expected a database url to be provided however none was defined".to_string());
+    let pool = PgPoolOptions::new().max_connections(3).acquire_timeout(std::time::Duration::from_secs(3)).connect(&database).await.expect("failed to connect to the database with the provided url");
 }
