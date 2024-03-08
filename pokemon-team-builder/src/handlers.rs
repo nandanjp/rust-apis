d42a191a59553a20da34ba66aefcfe46831bfcd5
nandanjp <nandan.jp17@gmail.com>
2024-03-08 16:12:30 -0700

    initialized the backend

--- pokemon-team-builder/src/handlers.rs
+++
@@ -1,7 +0,0 @@
-pub mod abilities;
-pub mod games;
-pub mod moves;
-pub mod pokedex;
-pub mod pokemon;
-pub mod team;
-pub mod users;
--- pokemon-team-builder/src/models/enums.rs
+++
@@ -1,7 +0,0 @@
-pub mod games;
-pub mod genders;
-pub mod natures;
-pub mod region;
-pub mod stats;
-pub mod tier;
-pub mod types;
