794aa14d4f5b4026f18acf0751ec7000dd5cdd1f
nandanjp <nandan.jp17@gmail.com>
2024-03-11 10:37:48 -0600

    re-initialized the blog application, and using sql now

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,101 +1,12 @@
-use crate::config::Config;
-use crate::models::common::DatabaseConfig;
-use crate::routes::blog::{create_blog, delete_blog, get_blog_by_id, get_blogs, update_blog};
-use crate::routes::category::{
-    create_category, delete_category, get_categories, get_category_by_id, update_category,
-};
-use crate::routes::image::{create_image, delete_image, get_image_by_id, get_images, update_image};
-use axum::response::IntoResponse;
-use axum::{
-    routing::{delete, get, post, put},
-    Router,
-};
-use http::StatusCode;
-use http::{header, HeaderValue};
-use mongodb::options::ClientOptions;
-use mongodb::Client;
-use std::time::Duration;
-use tower_http::limit::RequestBodyLimitLayer;
-use tower_http::set_header::SetResponseHeaderLayer;
-use tower_http::timeout::TimeoutLayer;
-use tower_http::trace::TraceLayer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-
-mod config;
-mod models;
-mod routes;
-mod utils;
-
-async fn handler_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "nothing to see here")
-}
-
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| {
-                "rust_axum=debug,axum=debug,tower_http=debug,mongodb=debug".into()
-            }),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+   dotenv::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("failed to find a database url");
+    let port = std::env::var("PORT").map(|p| p.parse::<u32>().unwrap()).expect("failed to find a valid port defined");
 
-    let config = Config::new();
-    let db_config = DatabaseConfig::new(config.mongo_config());
-    let mut client_options = ClientOptions::parse(db_config.uri).await.unwrap();
-    client_options.connect_timeout = Some(db_config.connection_timeout);
-    client_options.max_pool_size = Some(db_config.max_pool_size);
-    client_options.min_pool_size = Some(db_config.min_pool_size);
+    let pool = sqlx::postgres::PgPoolOptions::new().max_connections(3).acquire_timeout(std::time::Duration::from_secs(5)).connect(&db_url).await.expect(&format!("failed to connect to the database defined at the given address: {}", db_url));
+    let listener = tokio::net::TcpListener::bind(&format!("127.0.0.1:{}", port)).await.expect(&format!("failed to retrieve a tcp listener defined at the given port: {}", port));
+    let app = axum::Router::new().route("/health", axum::routing::get(|| async { "Server is healthy and listening" }));
 
-    let client = Client::with_options(client_options).unwrap();
-
-    let app = Router::new()
-        .route("/", get(|| async { "Server is healthy!" }))
-        .nest(
-            "/api/blog",
-            Router::new()
-                .route("/", get(get_blogs))
-                .route("/", post(create_blog))
-                .route("/:id", get(get_blog_by_id))
-                .route("/:id", put(update_blog))
-                .route("/:id", delete(delete_blog)),
-        )
-        .nest(
-            "/api/image",
-            Router::new()
-                .route("/", get(get_images))
-                .route("/", post(create_image))
-                .route("/:id", get(get_image_by_id))
-                .route("/:id", put(update_image))
-                .route("/:id", delete(delete_image)),
-        )
-        .nest(
-            "/api/category",
-            Router::new()
-                .route("/", get(get_categories))
-                .route("/", post(create_category))
-                .route("/:id", get(get_category_by_id))
-                .route("/:id", put(update_category))
-                .route("/:id", delete(delete_category)),
-        )
-        .layer(TimeoutLayer::new(Duration::from_secs(10)))
-        .layer(RequestBodyLimitLayer::new(1024))
-        .layer(TraceLayer::new_for_http())
-        .layer(SetResponseHeaderLayer::if_not_present(
-            header::SERVER,
-            HeaderValue::from_static("blog"),
-        ));
-    let app = app.fallback(handler_404).with_state(client);
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.user_config().port))
-        .await
-        .unwrap();
-    tracing::debug!(
-        "listening on {}",
-        format!("0.0.0.0:{}", config.user_config().port)
-    );
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap()
+    axum::serve(listener, app.with_state(pool).into_make_service()).await.expect("failed to serve the given app using the provided listener");
 }
