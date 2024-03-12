4217e2ddeb8d2894084fee99a6fc63dfdff47489
Nandan Patel <nandan.jp17@gmail.com>
2024-03-12 08:54:19 -0600

    updated

--- music-api/src/handlers/authenticate.rs
+++ music-api/src/handlers/authenticate.rs
@@ -1,16 +1,3 @@
-/*
-*curl -X POST "https://accounts.spotify.com/api/token" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
-* */
-
-/*
-*{
-  "access_token": "BQDBKJ5eo5jxbtpWjVOj7ryS84khybFpP_lTqzV7uV-T_m0cTfwvdn5BnBSKPxKgEb11",
-  "token_type": "Bearer",
-  "expires_in": 3600
-}
-* */
 use axum::{
     async_trait,
     extract::{FromRequestParts, State},
@@ -23,11 +10,33 @@ use axum_extra::{
     TypedHeader,
 };
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use once_cell::sync::Lazy;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::{config::Config, AppState};
+
+struct Keys {
+    encoding: EncodingKey,
+    decoding: DecodingKey,
+}
+
+impl Keys {
+    fn new(secret: &[u8]) -> Self {
+        Self {
+            encoding: EncodingKey::from_secret(secret),
+            decoding: DecodingKey::from_secret(secret),
+        }
+    }
+}
+
+static KEYS: Lazy<Keys> = Lazy::new(|| {
+    let secret = Config::new()
+        .expect("failed to parse the configuration stuff")
+        .jwt_token;
+    Keys::new(secret.as_bytes())
+});
 
 #[derive(Debug)]
 pub enum AuthError {
@@ -37,7 +46,7 @@ pub enum AuthError {
 }
 
 #[async_trait]
-impl<S> FromRequestParts<S> for Token
+impl<S> FromRequestParts<S> for Claims
 where
     S: Send + Sync,
 {
@@ -48,23 +57,17 @@ where
             .extract::<TypedHeader<Authorization<Bearer>>>()
             .await
             .map_err(|_| AuthError::InvalidToken)?;
-        // let config = Config::new().map_err(|_| AuthError::TokenCreation)?;
-        // let jwt_secret = DecodingKey::from_secret(config.jwt_token.as_bytes());
-        // let token_data =
-        //     decode::<AuthResponse>(bearer.token(), &jwt_secret, &Validation::default())
-        //         .map_err(|_| AuthError::InvalidToken)?;
+        let token_data =
+            match decode::<Claims>(bearer.token(), &KEYS.decoding, &Validation::default()) {
+                Ok(token) => token.claims,
+                Err(e) => {
+                    tracing::debug!("Failed to parse the jwt token: {}", e);
+                    println!("Failed to parse the jwt token: {}", e);
+                    return Err(AuthError::InvalidToken);
+                }
+            };
 
-        let key = DecodingKey::from_secret(&[]);
-        let mut validation = Validation::new(jsonwebtoken::Algorithm::HS256);
-        validation.insecure_disable_signature_validation();
-        let token = bearer.token();
-        let token_data: AuthResponse =
-            decode(&token, &key, &validation).map_err(|_| AuthError::InvalidToken)?;
-        let response = token_data.claims;
-        Ok(Self {
-            expires_in: response.expires_in,
-            token: response.access_token,
-        })
+        Ok(token_data)
     }
 }
 
@@ -90,9 +93,16 @@ pub struct AuthResponse {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Token {
-    pub token: String,
-    pub expires_in: u64,
+pub struct Claims {
+    pub sub: String,
+    pub company: String,
+    pub exp: usize,
+}
+
+impl std::fmt::Display for Claims {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Token: {}\nExpires In: {}", self.sub, self.company)
+    }
 }
 
 pub async fn authorize(State(app): State<AppState>) -> Result<impl IntoResponse, AuthError> {
@@ -115,21 +125,18 @@ pub async fn authorize(State(app): State<AppState>) -> Result<impl IntoResponse,
             Ok(res) => {
                 let mut headers = HeaderMap::new();
                 headers.insert("Content-Type", "application/json".parse().unwrap());
-                headers.insert(
-                    "Authorization",
-                    res.access_token.clone().as_str().parse().unwrap(),
-                );
 
-                let encoding = EncodingKey::from_secret(app.jwt_secret.as_bytes());
-                let token = encode(&Header::default(), &res, &encoding)
+                let claims = Claims {
+                    sub: res.access_token.clone(),
+                    company: res.access_token.clone(),
+                    exp: 2400000000,
+                };
+
+                let token = encode(&Header::default(), &claims, &KEYS.encoding)
                     .map_err(|_| AuthError::TokenCreation)?;
-                Ok((
-                    headers,
-                    Json(Token {
-                        expires_in: res.expires_in,
-                        token,
-                    }),
-                ))
+
+                headers.insert("Authorization", token.clone().as_str().parse().unwrap());
+                Ok((headers, Json(claims)))
             }
             Err(_) => Err(AuthError::InvalidToken),
         },
@@ -138,29 +145,68 @@ pub async fn authorize(State(app): State<AppState>) -> Result<impl IntoResponse,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ExplicitContent {
+    filter_enabled: bool,
+    filter_locked: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Followers {
+    href: String,
+    total: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Image {
+    url: String,
+    height: usize,
+    width: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     country: String,
     display_name: String,
     email: String,
+    explicit_content: ExplicitContent,
+    external_urls: ExternalUrls,
+    followers: Followers,
     href: String,
     id: String,
+    images: Vec<Image>,
+    product: String,
     #[serde(rename = "type")]
     type_: String,
     uri: String,
 }
-pub async fn get_user(token: Token) -> Result<impl IntoResponse, AuthError> {
-    println!("user = {:#?}", token.clone());
+pub async fn get_user(token: Claims) -> Result<impl IntoResponse, AuthError> {
     let res = Client::new()
         .get("https://api.spotify.com/v1/me")
-        .header("Authorization", format!("Bearer: {}", token.token).as_str())
+        .header("Authorization", format!("Bearer {}", token.sub.clone()))
         .send()
-        .await;
+        .await
+        .map_err(|_| AuthError::WrongCredentials)?;
+    println!("res = {:#?}", res);
 
-    match res {
-        Ok(res) => match res.json::<User>().await {
-            Ok(res) => Ok((StatusCode::OK, Json(res))),
-            Err(_) => Err(AuthError::InvalidToken),
-        },
-        Err(_) => Err(AuthError::WrongCredentials),
+    let text = res.text().await.map_err(|_| AuthError::InvalidToken)?;
+    println!("res.body: {}", text);
+
+    let res = Client::new()
+        .get("https://api.spotify.com/v1/me")
+        .header("Authorization", format!("Bearer {}", token.sub))
+        .send()
+        .await
+        .map_err(|_| AuthError::WrongCredentials)?;
+    match res.json::<User>().await {
+        Ok(res) => Ok((StatusCode::OK, Json(res))),
+        Err(e) => {
+            println!("error in unwrapping = {e}");
+            Err(AuthError::InvalidToken)
+        }
     }
 }
